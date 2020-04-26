745de22876e8b068038857d3af6d62a35d2f97f8
David Zaslavsky <diazona@ellipsix.net>
2020-04-26 14:01:45 -0700

    Stream output back to the client
    This commit adds code that streams the standard output and standard
    error of a program being run back to the client. This requires using
    a client that is set up to receive the streamed data.

--- run-remote.py
+++ run-remote.py
@@ -4,33 +4,47 @@ import asyncio
 import shlex
 import sys
 
-async def copy_output(process, stream, prefix):
+async def copy_output(process, source_stream, destination_stream):
     while process.returncode is None:
-        data = await stream.readline()
+        data = await source_stream.readline()
         if not data:
-            if stream.at_eof():
+            if source_stream.at_eof():
                 break
             else:
                 continue
-        print('{}: {}'.format(prefix, data.decode('ascii').rstrip()))
+        destination_stream.write(data)
+        await destination_stream.drain()
 
-async def run(program, *args):
+async def run(program, *args, output_stream=None, errput_stream=None):
+    if output_stream is None:
+        stdout_destination = asyncio.subprocess.DEVNULL
+    else:
+        stdout_destination = asyncio.subprocess.PIPE
+    if errput_stream is None:
+        stderr_destination = asyncio.subprocess.DEVNULL
+    else:
+        stderr_destination = asyncio.subprocess.PIPE
     process = await asyncio.create_subprocess_exec(
         program,
         *args,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE # TODO maybe change this
+        stdout=stdout_destination,
+        stderr=stderr_destination
     )
-    await asyncio.gather(
-        copy_output(process, process.stdout, f'[{program}:1]'),
-        copy_output(process, process.stderr, f'[{program}:2]')
-    )
+    io_tasks = []
+    if output_stream is not None:
+        io_tasks.append(asyncio.create_task(copy_output(process, process.stdout, output_stream)))
+    if errput_stream is not None:
+        io_tasks.append(asyncio.create_task(copy_output(process, process.stderr, errput_stream)))
+    if io_tasks:
+        await asyncio.gather(*io_tasks)
+    else:
+        await process.wait()
     print(f'[{program}] <exited with code {process.returncode}>')
 
 async def command_loop(reader, writer):
     data = await reader.readline()
     program, *args = shlex.split(data.decode('ascii').rstrip())
-    await run(program, *args)
+    await run(program, *args, output_stream=writer, errput_stream=writer)
 
 async def main():
     server = await asyncio.start_server(command_loop, host='localhost', port=13180) # same port from RemoteForward SSH directive (see ~/.ssh/config)
