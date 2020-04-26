53d5f6fe9d5cbe6d67bf6c735a76828a21910acc
David Zaslavsky <diazona@ellipsix.net>
2020-04-26 16:04:50 -0700

    Update the client to use argparse
    This commit adds support for option parsing to the client and creates
    a 'run' subcommand that needs to be used to run the client. It also
    adds options to specify the host and port. This will help with combining
    the server and client scripts later on.

--- run-remote-client.py
+++ run-remote-client.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.8
 
+import argparse
 import asyncio
 import shlex
 import sys
@@ -14,22 +15,35 @@ async def print_output(source_stream):
                 continue
         print(data.decode('ascii').rstrip())
 
-async def run(port, program, *args):
+async def run(host, port, program, *args):
     command = shlex.join([program] + list(args))
     print(f'[{program}] <starting>')
-    reader, writer = await asyncio.open_connection('localhost', port)
+    reader, writer = await asyncio.open_connection(host, port)
     writer.write((command + '\n').encode('ascii'))
     await writer.drain()
     await print_output(reader)
     writer.close()
     print(f'[{program}] <exited>')
 
-async def main():
-    port = int(sys.argv[1])
-    await run(port, *sys.argv[2:])
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+
+    subparsers = parser.add_subparsers(required=True, dest='subcommand', title='subcommands')
+    run_parser = subparsers.add_parser('run', help='Run a command on the remote host')
+    run_parser.add_argument('--host', action='store', default='localhost', help='Hostname or IP address to connect to')
+    run_parser.add_argument('-p', '--port', action='store', type=int, default=13180, help='Port to connect to')
+    run_parser.add_argument('command', help='Command to run')
+    run_parser.add_argument('arguments', nargs=argparse.REMAINDER, help='Arguments for the command')
+
+    return parser.parse_args()
+
+def main():
+    args = parse_arguments()
+    if args.subcommand == 'run':
+        try:
+            asyncio.run(run(args.host, args.port, args.command, *args.arguments))
+        except KeyboardInterrupt:
+            pass # TODO wait for process
 
 if __name__ == '__main__':
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        pass # TODO wait for process
+    main()
