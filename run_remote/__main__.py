6705fb45f6faf18f036f8921ac52099a3d71942a
David Zaslavsky <diazona@ellipsix.net>
2020-04-26 23:27:50 -0700

    Refactor out top-level code into a __main__ module
    This commit pulls out the code from the client and server which would
    be invoked when they were run from the command line, and puts it in
    a common __main__ module which will delegate to either the client or
    server depending on the subcommand it gets.

---
+++ run_remote/__main__.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3.8
+
+import argparse
+import asyncio
+from .client import run
+from .server import serve
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+
+    subparsers = parser.add_subparsers(required=True, dest='subcommand', title='subcommands')
+
+    serve_parser = subparsers.add_parser('serve', help='Start the server')
+    serve_parser.add_argument('--host', action='store', default='localhost', help='Hostname or IP address on which to run the server')
+    serve_parser.add_argument('-p', '--port', action='store', type=int, default=13180, help='Port on which to run the server') # this port should be used in the RemoteForward SSH directive (see ~/.ssh/config)
+
+    run_parser = subparsers.add_parser('run', help='Run a command on the remote host')
+    run_parser.add_argument('--host', action='store', default='localhost', help='Hostname or IP address to connect to')
+    run_parser.add_argument('-p', '--port', action='store', type=int, default=13180, help='Port to connect to')
+    run_parser.add_argument('command', help='Command to run')
+    run_parser.add_argument('arguments', nargs=argparse.REMAINDER, help='Arguments for the command')
+
+    return parser.parse_args()
+
+def main():
+    args = parse_arguments()
+    if args.subcommand == 'serve':
+        coroutine = serve(args.host, args.port)
+    elif args.subcommand == 'run':
+        coroutine = run(args.host, args.port, args.command, *args.arguments)
+    else:
+        assert(False) # argparse should have caught this
+    try:
+        asyncio.run(coroutine)
+    except KeyboardInterrupt:
+        pass # TODO wait for processes
+
+if __name__ == '__main__':
+    main()
--- run_remote/client.py
+++ run_remote/client.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3.8
-
-import argparse
 import asyncio
 import shlex
 import sys
@@ -24,26 +21,3 @@ async def run(host, port, program, *args):
     await print_output(reader)
     writer.close()
     print(f'[{program}] <exited>')
-
-def parse_arguments():
-    parser = argparse.ArgumentParser()
-
-    subparsers = parser.add_subparsers(required=True, dest='subcommand', title='subcommands')
-    run_parser = subparsers.add_parser('run', help='Run a command on the remote host')
-    run_parser.add_argument('--host', action='store', default='localhost', help='Hostname or IP address to connect to')
-    run_parser.add_argument('-p', '--port', action='store', type=int, default=13180, help='Port to connect to')
-    run_parser.add_argument('command', help='Command to run')
-    run_parser.add_argument('arguments', nargs=argparse.REMAINDER, help='Arguments for the command')
-
-    return parser.parse_args()
-
-def main():
-    args = parse_arguments()
-    if args.subcommand == 'run':
-        try:
-            asyncio.run(run(args.host, args.port, args.command, *args.arguments))
-        except KeyboardInterrupt:
-            pass # TODO wait for process
-
-if __name__ == '__main__':
-    main()
