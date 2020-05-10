d90baa27f4d9e098aad394357bc7dabe0ec34ae5
David Zaslavsky <diazona@ellipsix.net>
2020-05-09 23:38:56 -0700

    Switch to a general TOML-format configuration file
    This commit changes from using a config file for just the logging
    system to a general config file that will apply to the whole program.
    The configuration file is now a TOML file instead of Python's
    configparser format.
    
    The main reason for this change is that the logging module's
    documentation says that the fileConfig() method is somewhat deprecated,
    or at least may not be updated with future changes, so I wanted to
    switch to the more future-proof dictConfig() method. Given that, there's
    no reason for the configuration to be in the configparser format. TOML
    is a more modern and more precisely defined format, which seems to be
    becoming somewhat of a standard for Python projects, so it looked like
    a particularly suitable choice.

--- run_remote/__main__.py
+++ run_remote/__main__.py
@@ -6,8 +6,7 @@ import logging
 import logging.config
 import logging.handlers
 import sys
-from .client import run
-from .server import serve
+import tomlkit
 
 def log_destination(s):
     if s in ('syslog', 'stderr'):
@@ -21,8 +20,8 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-v', '--verbose', action='count', help='Enable verbose output; repeat for increased verbosity')
+    parser.add_argument('--config', metavar='FILE', action='store', help='Read configuration from a file')
     parser.add_argument('--log-dest', metavar='DEST', type=log_destination, help='Set the destination for verbose output; valid values are stderr or file:FILENAME')
-    parser.add_argument('--log-config', metavar='FILE', action='store', help='Configure the logging system from a file')
 
     subparsers = parser.add_subparsers(required=True, dest='subcommand', title='subcommands')
 
@@ -38,33 +37,46 @@ def parse_arguments():
 
     return parser.parse_args()
 
-def configure_logging(args):
-    if args.log_config:
-        logging.fileConfig(args.log_config)
-        return
+def load_configuration(filename):
+    if not filename:
+        return {}
+    with open(filename) as f:
+        return tomlkit.parse(f.read())
+
+def configure_logging(configuration, verbosity=None, log_dest=None):
+    if 'logging' in configuration:
+        logging.config.dictConfig(configuration['logging'])
 
     basic_config_parameters = {}
-    if args.verbose == 1:
+    if verbosity == 1:
         basic_config_parameters['level'] = logging.WARNING
-    elif args.verbose == 2:
+    elif verbosity == 2:
         basic_config_parameters['level'] = logging.INFO
-    elif args.verbose == 3:
+    elif verbosity == 3:
         basic_config_parameters['level'] = logging.DEBUG
-    if args.log_dest == 'stderr':
+    if log_dest == 'stderr':
         basic_config_parameters['handlers'] = [logging.StreamHandler()]
-    elif args.log_dest and args.log_dest.startswith('file:'):
-        basic_config_parameters['handlers'] = [logging.FileHandler(args.log_dest[5:])]
-    logging.basicConfig(**basic_config_parameters)
+    elif log_dest and log_dest.startswith('file:'):
+        basic_config_parameters['handlers'] = [logging.FileHandler(log_dest[5:])]
+    if basic_config_parameters:
+        logging.basicConfig(**basic_config_parameters)
 
 def main():
     args = parse_arguments()
-    configure_logging(args)
+    configuration = load_configuration(args.config)
+    configure_logging(configuration, args.verbose, args.log_dest)
     if args.subcommand == 'serve':
+        # This import has to be here so that the logging system gets configured
+        # before the module is imported and its corresponding logger is loaded
+        from run_remote.server import serve
         try:
             asyncio.run(serve(args.host, args.port))
         except KeyboardInterrupt:
             pass # TODO wait for processes
     elif args.subcommand == 'run':
+        # This import has to be here so that the logging system gets configured
+        # before the module is imported and its corresponding logger is loaded
+        from run_remote.client import run
         try:
             exit_code = asyncio.run(run(args.host, args.port, args.command, *args.arguments))
             if exit_code is None:
