23dc9d0411e617f19369168f40126ff391bae313
David Zaslavsky <diazona@ellipsix.net>
2020-05-17 15:44:13 -0700

    Add type annotations and fix a few related bugs
    This commit adds type annotations to the code and fixes a few minor bugs
    that were found by running mypy on the annotated code - things like
    missing return values and type mismatches.

--- run_remote/command.py
+++ run_remote/command.py
@@ -1,16 +1,22 @@
 import shutil
+from typing import Callable, Container, Iterable, Optional, Tuple
 
-def default_sanitizer(program, args):
+Command = str
+Arguments = Iterable[str]
+
+def default_sanitizer(program: Command, args: Arguments) -> Optional[Tuple[Command, Arguments]]:
     program = shutil.which(program) or program
     return (program, args) if program else None
 
 class PathProgramWhitelist:
-    def __init__(self, allowed_programs):
+    def __init__(self, allowed_programs: Container[Command]) -> None:
         self.allowed_programs = allowed_programs
 
-    def __call__(self, program, args):
+    def __call__(self, program: Command, args: Arguments):
         if program in self.allowed_programs:
-            program = shutil.which(program)
-            return (program, args) if program else None
+            qualified_program: Optional[str] = shutil.which(program)
+            return (qualified_program, args) if qualified_program else None
         else:
             return None
+
+CommandSanitizer = Callable[[Command, Arguments], Optional[Tuple[Command, Arguments]]]
